e53e2883aeb1b2ef8ecdb0b78ba2eb85bd28d1be
Casey <thayer.caseyl@gmail.com>
2022-03-22 19:56:06 -0700

    final code and readMe file uploaded

--- Module 19 Challenge/Starter_Code/fintech_finder.py
+++ Module 19 Challenge/Starter_Code/fintech_finder.py
@@ -79,7 +79,7 @@ w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
 # @TODO:
 # From `crypto_wallet.py import the functions generate_account, get_balance,
 #  and send_transaction
-# YOUR CODE HERE
+from crypto_wallet import generate_account, get_balance, send_transaction
 
 ################################################################################
 # Fintech Finder Candidate Information
@@ -130,7 +130,7 @@ st.sidebar.markdown("## Client Account Address and Ethernet Balance in Ether")
 
 # @TODO:
 #  Call the `generate_account` function and save it as the variable `account`
-# YOUR CODE HERE
+account = generate_account()
 
 ##########################################
 
@@ -139,14 +139,20 @@ st.sidebar.write(account.address)
 
 ##########################################
 # Step 1 - Part 5:
-# Define a new `st.sidebar.write` function that will display the balance of the
-# customer’s account. Inside this function, call the `get_balance` function and
-#  pass it your Ethereum `account.address`.
 
 # @TODO
 # Call `get_balance` function and pass it your account address
 # Write the returned ether balance to the sidebar
-# YOUR CODE HERE
+
+
+ether = get_balance(w3, account.address)
+
+st.sidebar.markdown("## Your Balance of Ether")
+st.sidebar.write(ether)
+st.sidebar.write("---------")
+
+
+
 
 ##########################################
 
@@ -237,11 +243,11 @@ st.sidebar.markdown("## Total Wage in Ether")
 # Calculate total `wage` for the candidate by multiplying the candidate’s hourly
 # rate from the candidate database (`candidate_database[person][3]`) by the
 # value of the `hours` variable
-# YOUR CODE HERE
+wage = (candidate_database[person][3]) * hours
 
 # @TODO
 # Write the `wage` calculation to the Streamlit sidebar
-# YOUR CODE HERE
+st.sidebar.write(wage)
 
 ##########################################
 # Step 2 - Part 2:
@@ -268,7 +274,7 @@ if st.sidebar.button("Send Transaction"):
     # Call the `send_transaction` function and pass it 3 parameters:
     # Your `account`, the `candidate_address`, and the `wage` as parameters
     # Save the returned transaction hash as a variable named `transaction_hash`
-    # YOUR CODE HERE
+    transaction_hash = send_transaction(w3, account, candidate_address, wage)
 
     # Markdown for the transaction hash
     st.sidebar.markdown("#### Validated Transaction Hash")
@@ -281,7 +287,7 @@ if st.sidebar.button("Send Transaction"):
 
 # The function that starts the Streamlit application
 # Writes FinTech Finder candidates to the Streamlit page
-get_people()
+get_people(w3)
 
 ################################################################################
 # Step 3: Inspect the Transaction
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# module_19_challenge
+# Module 19 challenge
+
+This a program that uses code to enable our customers to hire fintech professionals by sending them cryptocurrency.
+
+This program is broken down by 2 essential files:
+    - cryptocurrency wallet that handles the transferring and receiving of funds
+    - fintech finder file that creates the decentralized web page and creates a smooth user interface
+
+The streamlit app uses a simple process:
+    -pick fintech propfessional that fits criteria for worka and desired rate
+    -input desired time of work
+    -confirm amount of eth required for transaction and send eth
+
+Attached are pictures of the transaction that has been ran through ganache and the balances sent for required services. 
+
+!["Ganache1"](module19_ganache.PNG)
+!["Ganache2"](module19_ganache2.PNG)
+!["Ganache3"](module19_ganache3.PNG)
